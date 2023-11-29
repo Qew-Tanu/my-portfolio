040ba7b5a3b4e50bf75100a9fbbbe74239a0ad39
Tanu Tarathikhampor <qewtanu@hotmail.com>
2023-11-29 23:02:03 +0700

    update contact

--- src/Components/contact/Contacts.jsx
+++ src/Components/contact/Contacts.jsx
@@ -8,10 +8,10 @@ const Contacts = () => {
         <div id='Contact' className='flex flex-col w-full items-center border mt-5 border-black border-solid rounded-[3em] p-5 gap-10'>
             <TextHeader>Contact</TextHeader>
             <div className='flex w-full justify-center'>
-                <div className=' sm:h-full flex flex-col p-5 gap-4'>
+                <div className=' sm:h-full flex flex-col p-5 gap-4 '>
                     {contactItems.map((item, index) => {
                         return (
-                            <a href={item?.link} className=' flex gap-2 items-center justify-start ' key={index}>
+                            <a href={item?.link} className=' flex gap-2 items-center justify-start rounded-[2em] hover:bg-blue-200 p-3 ' key={index}>
                                 <img src={item.image} className='w-[2em] h-[2em]' />
                                 <div className='font-semibold min-w-[55px] md:min-w-[85px]'>{item.name} : </div>
                                 <p className='font-semibold truncate overflow-hidden break-keep'>{item.link ? item.link : item.detail}</p>
